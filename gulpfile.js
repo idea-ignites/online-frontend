f7b3116dd4abb2082f60922b1365fbf80e4cf2db
hsiaofongw <hsiaofong.w@gmail.com>
2020-06-22 17:20:34 +0800

    update

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const { src, dest, watch, series } = require('gulp');
+const ts = require('gulp-typescript');
+const tsProject = ts.createProject('tsconfig.json');
+const tsify = require('tsify');
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+
+function build() {
+    return browserify({
+        basedir: '.',
+        debug: true,
+        entries: ['main.ts']
+    })
+    .plugin(tsify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(dest('.'));
+}
+
+function onSourceChanged() {
+    return build();
+}
+
+function watchFileChange() {
+    watch('*.ts', onSourceChanged);
+}
+
+exports.default = watchFileChange;
